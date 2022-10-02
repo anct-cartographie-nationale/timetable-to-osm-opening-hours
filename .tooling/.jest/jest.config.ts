85d3b03977119553ba3593d4b067b4d0555a463f
Marc Gavanier <marc.gavanier@gmail.com>
2022-10-02 17:14:58 +0200

    chore: initial commit

---
+++ .tooling/.jest/jest.config.ts
@@ -0,0 +1,20 @@
+// From https://kulshekhar.github.io/ts-jest/docs/getting-started/presets
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+  rootDir: './../../src',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  //
+  transform: {
+    // This is the default transform regex
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './.tsconfig/tsconfig.test.json'
+      }
+    ]
+  }
+};
+
+export default jestConfig;
